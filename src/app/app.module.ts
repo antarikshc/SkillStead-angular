c793940cbce1d09873ba444893894ef734e50045
Antariksh C <antarikshc@gmail.com>
2019-03-14 22:37:42 +0530

    feat: Add HomeComponent - Landing page with UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogoComponent } from './logo/logo.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
