828d42d403efc5f0d3c497622a054b58dec698c3
Antariksh C <antarikshc@gmail.com>
2019-03-17 02:31:01 +0530

    refactor: Rename Login -> SignUp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LogoComponent } from './logo/logo.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './home/login/login.component';
 import { MaterialModule } from './material.module';
+import { SignUpComponent } from './home/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/signup/signup.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: [
+    './signup.component.css',
+    './signup.theme.scss'
+]
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
