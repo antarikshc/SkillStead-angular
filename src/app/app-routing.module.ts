8e9562c6cbabac96d3b225b22c452893920b2966
Antariksh C <antarikshc@gmail.com>
2019-03-17 02:55:40 +0530

    feat: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import { SignUpComponent } from './home/signup/signup.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent}
 ];
 
 @NgModule({
